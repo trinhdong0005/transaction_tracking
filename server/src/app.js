b2987f2b43e83d40c09597b29a8a600bf621de21
Dong Dev <admin@dongdev.com>
2025-09-17 16:42:32 +0700

    Feat: Implement API key middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { notFound, errorHandler } = require('./middlewares')
+const { notFound, errorHandler, apiKeyAuthMiddleware } = require('./middlewares')
 const transaction = require('./routes/transaction')
 
 const app = express()
@@ -13,5 +13,6 @@ app.use('/api/v1/transaction', transaction)
 
 app.use(notFound)
 app.use(errorHandler)
+app.use(apiKeyAuthMiddleware)
 
 module.exports = app
